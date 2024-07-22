f4227f87480a4e0a7c91c742d331162b2ac1bdcb
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-07-22 11:35:39 +0500

    commit

--- FrontEndPills/app/(auth)/Register.tsx
+++ FrontEndPills/app/(auth)/Register.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "@/navigation/StackNav";
 
-const Register = () => {
+interface IRegister {
+  navigation:StackNavigationProp<RootStackParamList,'Register'>
+}
+
+const Register = ({navigation}:IRegister) => {
+  const handlerPress = ()=>{
+      navigation.navigate('VerificationCode')
+  }
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Register Screen</Text>
+      <TouchableOpacity onPress={handlerPress}>
+        <Text>Goto Verification</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- FrontEndPills/app/(auth)/VerificationCode.tsx
+++ FrontEndPills/app/(auth)/VerificationCode.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const VerificationCode = () => {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center">
       <Text>VerificationCode</Text>
     </View>
   )
