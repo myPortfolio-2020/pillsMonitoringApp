73ab834ef66e8e4d639e24144d05ff01979fc44c
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-07-23 18:48:39 +0500

    uyhi

--- FrontEndPills/app/screens/UserScreen.tsx
+++ FrontEndPills/app/screens/UserScreen.tsx
@@ -1,32 +1,12 @@
-import { View, Text,TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
-import { StackNavigationProp } from '@react-navigation/stack'
-import { PatientStackParamList } from '@/navigation/PatientsStack'
 
-interface IUserScreenProp {
-  navigation: StackNavigationProp<PatientStackParamList, 'UserScreen'>
-}
-
-const UserScreen = ({navigation}:IUserScreenProp) => {
-    const handlerPress = ()=>{
-      navigation.navigate('CreateNewPatient')
-      // navigation.push('CreateNewPatient')
-    }
+const UserScreen = () => {
   return (
-     <View>
-      <TouchableOpacity onPress={handlerPress}>
-        <Text style={styles.createBtn}>Create New Patient btn</Text>
-      </TouchableOpacity> 
+    <View>
+      <Text>UserScreen</Text>
     </View>
   )
 }
 
 export default UserScreen
-
-const styles = StyleSheet.create({
-    createBtn:{
-        backgroundColor:'#ddff33',        
-        padding:9,
-        margin:9
-    }
-})
