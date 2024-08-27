a9db0f4760619f6c37a010b2edaec1e0f0f65280
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-08-27 12:31:48 +0500

    commit

---
+++ PillsTrackFrontEnd/app/navigation/CustomTabBar.tsx
@@ -0,0 +1,115 @@
+// src/App.tsx
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+const CustomTabBar: React.FC<BottomTabBarProps> = ({
+  state,
+  descriptors,
+  navigation,
+}) => {
+  return (
+    <View style={styles.tabBarContainer}>
+      <View style={styles.leftTabsContainer}>
+        {state.routes.slice(0, 3).map((route, index) => {
+          const { options } = descriptors[route.key];
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <TouchableOpacity
+              key={route.key}
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={styles.tabButton}
+            >
+              <Ionicons
+                name={
+                  route.name === 'Home'
+                    ? 'home-outline'
+                    : route.name === 'Search'
+                    ? 'search-outline'
+                    : 'person-outline'
+                }
+                size={24}
+                color={isFocused ? 'tomato' : 'gray'}
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+      <TouchableOpacity
+        style={styles.floatingButton}
+        onPress={() => navigation.navigate('CreateNewPatient')}
+      >
+        <Ionicons name="add" size={30} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CustomTabBar;
+
+
+const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    tabBarContainer: {
+      flexDirection: 'row',
+      height: 60,
+      borderTopWidth: 1,
+      borderTopColor: '#ccc',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    leftTabsContainer: {
+      flexDirection: 'row',
+      flex: 1,
+      justifyContent: 'space-around',
+    },
+    tabButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    floatingButton: {
+      width: 40,
+      height: 40,
+      borderRadius: 30,
+      backgroundColor: 'tomato',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      right: 20,
+      bottom: 80,
+      elevation: 5,
+    },
+  });
+  
