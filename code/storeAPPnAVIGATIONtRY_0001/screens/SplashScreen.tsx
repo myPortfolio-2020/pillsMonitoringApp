ce0123b3ea1438a595e560cae6fea5bd93cfd545
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-08-27 19:03:15 +0500

    commit

---
+++ code/storeAPPnAVIGATIONtRY_0001/screens/SplashScreen.tsx
@@ -0,0 +1,32 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { RootStackParamList } from "../navigation/StackNavigation";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+interface SplashProps {
+  navigation: NativeStackNavigationProp<RootStackParamList, "SplashScreen">;
+}
+
+const SplashScreen = ({ navigation }: SplashProps) => {
+  const handlerPress = () => {
+    navigation.navigate("Signup");
+  };
+  return (
+    <View className="flex-1 justify-center items-center ">
+      <Text>SplashScreen</Text>
+      <TouchableOpacity>
+        <Text onPress={handlerPress}>Start</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
