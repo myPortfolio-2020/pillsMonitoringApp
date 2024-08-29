08dba61b6004e9f1a0d13756306a5e67626080f2
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-08-29 18:56:25 +0500

    wer

--- PillsTrackFrontEnd/app/_layout.tsx
+++ PillsTrackFrontEnd/app/_layout.tsx
@@ -1,10 +1,40 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text,StyleSheet } from "react-native";
+import React,{useEffect} from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from "./navigation/StackNavigation";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 const _layout = () => {
+  const [fontsLoaded, error] = useFonts({
+    'Inter_18pt-ThinItalic': require('./../assets/fonts/Inter_18pt-ThinItalic.ttf'),
+    'Inter_18pt-MediumItalic': require('./../assets/fonts/Inter_18pt-MediumItalic.ttf'),
+    'Inter_18pt-Light': require('./../assets/fonts/Inter_18pt-Light.ttf'),
+    'Inter_18pt-Italic': require('./../assets/fonts/Inter_18pt-Italic.ttf'),
+    'Inter_18pt-ExtraBoldItalic': require('./../assets/fonts/Inter_18pt-ExtraBoldItalic.ttf'),
+    'Inter_18pt-BoldItalic': require('./../assets/fonts/Inter_18pt-BoldItalic.ttf'),
+    'Inter_18pt-BlackItalic': require('./../assets/fonts/Inter_18pt-BlackItalic.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
   return <>
   <NavigationContainer independent={true}>
     <StackNavigation/>
--- PillsTrackFrontEnd/tailwind.config.js
+++ PillsTrackFrontEnd/tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#1BB9E6',
+      },
+      fontFamily:{
+        fThinItalic: ["Inter_18pt-ThinItalic", "sans-serif"],
+        fThinItalic: ["Inter_18pt-MediumItalic", "sans-serif"],
+        fThinItalic: ["Inter_18pt-Light", "sans-serif"],
+        fThinItalic: ["Inter_18pt-Italic", "sans-serif"],
+        fThinItalic: ["Inter_18pt-ExtraBoldItalic", "sans-serif"],
+        fThinItalic: ["Inter_18pt-BoldItalic", "sans-serif"],
+        fThinItalic: ["Inter_18pt-BlackItalic", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
