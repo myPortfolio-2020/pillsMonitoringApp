c29a9b6a64f13d71835fa00d05996373c4af1c86
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-09-02 18:21:56 +0500

    done

--- PillsTrackFrontEnd/app/(tabs)/Home.tsx
+++ PillsTrackFrontEnd/app/(tabs)/Home.tsx
@@ -1,20 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 import { useGetPatientsQuery } from "../redux/slices/api";
 
 const Home = () => {
   const { data } = useGetPatientsQuery();
+  console.log("what comes here", data);
   return (
     <View>
-      <Text>Today’s medicine</Text>
+      {/* <Text>Today’s medicine</Text>
       <Text>Patient upcoming Pill Reminder</Text>
-
       <View>
         <Text>Patient Name: </Text>
         <Text>Drug Name: </Text>
         <Text>Time Remaining: </Text>
         <Text>Status: </Text>
-      </View>
+      </View> */}
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Patient Name: {item.userName}</Text>
+            <Text>userCategory: {item.userCategory}</Text>
+            <Text>userId: {item.userId}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
--- PillsTrackFrontEnd/app/redux/slices/api.ts
+++ PillsTrackFrontEnd/app/redux/slices/api.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001/0",
+    baseUrl: "http://localhost:3001",
   }),
   endpoints: (builder) => ({
     getPatients: builder.query<IPatient[], void>({
