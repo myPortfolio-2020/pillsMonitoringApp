f5b483ec695c1c12e0c10c65ff29fc085e86721f
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-09-03 15:32:14 +0500

    done

--- PillsTrackFrontEnd/app/auth/Signup.tsx
+++ PillsTrackFrontEnd/app/auth/Signup.tsx
@@ -1,7 +1,8 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/StackNavigation";
+import globalStyles from './../../globalStyles'
 
 interface SignupProp {
   navigation: NativeStackNavigationProp<RootStackParamList, "Signup">;
@@ -12,18 +13,22 @@ const Signup = ({ navigation }: SignupProp) => {
     navigation.navigate("VerificationCode");
   };
   return (
-    <View className="flex-1 justify-center"> 
-    <View className="justify-center">
-      <View className="items-end">
-        <Text>Already a Member? Sign In</Text>
-      </View>
+    <View className="flex-1 justify-center" style={globalStyles.container}>
+      <View className="justify-center">
+        <View className="items-end">
+          <Text>Already a Member? Sign In</Text>
+        </View>
 
-      <TouchableOpacity onPress={handlerPress}>
-        <Text>Verification Code</Text>
-      </TouchableOpacity>
-    </View>
+        <Pressable onPress={handlerPress}>
+          <Text>Verification Code</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 export default Signup;
+
+const styles = StyleSheet.create({
+
+})
---
+++ PillsTrackFrontEnd/globalStyles.ts
@@ -0,0 +1,11 @@
+// Global styleSheet
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,    
+  }
+});
+
+export default globalStyles;
