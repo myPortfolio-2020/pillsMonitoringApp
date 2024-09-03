c94950700d3a9a51d129a8d92714f4f571da41e8
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-09-03 18:46:00 +0500

    done

--- PillsTrackFrontEnd/app/auth/Signup.tsx
+++ PillsTrackFrontEnd/app/auth/Signup.tsx
@@ -1,15 +1,36 @@
-import { View, Text, StyleSheet, Pressable, Image, TextInput } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Image,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/StackNavigation";
 import globalStyles from "./../../globalStyles";
 import images from "./../../constants/images";
+import FormInput from "../components/FormInput";
 
 interface SignupProp {
   navigation: NativeStackNavigationProp<RootStackParamList, "Signup">;
 }
 
+interface IFields {
+  username: string;
+  password: string;
+  usernameError: string;
+  passwordError: string;
+}
+
 const Signup = ({ navigation }: SignupProp) => {
+  const [form, setForm] = useState<IFields>({
+    username: "",
+    password: "",
+    usernameError: "",
+    passwordError: "",
+  });
   const handlerPress = () => {
     navigation.navigate("VerificationCode");
   };
@@ -24,10 +45,40 @@ const Signup = ({ navigation }: SignupProp) => {
           <Text>SignIn</Text>
           <Text>Please enter details to register</Text>
         </View>
-        <View>
+        {/* <View>
           <Text>Name</Text>
           <TextInput style={globalStyles.textInput} placeholder="Enter Name" placeholderTextColor="#BDC2C3" />
-        </View>
+        </View> */}
+        <FormInput
+          label="Name"
+          value={form.username}
+          onChangeText={(e) => setForm({ ...form, username: e })}
+          placeholder="Enter your name"
+        />
+        <FormInput
+          label="Email"
+          value={form.password}
+          onChangeText={(e) => setForm({ ...form, password: e })}
+          placeholder="Enter your email"
+        />
+        <FormInput
+          label="Mobile number"
+          value={form.password}
+          onChangeText={(e) => setForm({ ...form, password: e })}
+          placeholder="Enter your mobile number"
+        />
+        <FormInput
+          label="Password"
+          value={form.password}
+          onChangeText={(e) => setForm({ ...form, password: e })}
+          placeholder="Enter your password"
+        />
+        <FormInput
+          label="Confirm password"
+          value={form.password}
+          onChangeText={(e) => setForm({ ...form, password: e })}
+          placeholder="Confirm password"
+        />
         <Pressable onPress={handlerPress}>
           <Text style={globalStyles.fullSpreadBtn}>Sign up</Text>
         </Pressable>
