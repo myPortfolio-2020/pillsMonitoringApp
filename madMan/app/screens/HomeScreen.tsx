068f2cc750e0c346f27c016a3bfcfa17484c4264
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-09-10 14:01:19 +0500

    ddd

--- madMan/app/screens/HomeScreen.tsx
+++ madMan/app/screens/HomeScreen.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./Home";
+import Patient from "./Patient";
+import Track from "./Track";
+
+const Tab = createBottomTabNavigator();
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Track" component={Track} />
+      <Tab.Screen name="Patient" component={Patient} />
+    </Tab.Navigator>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
