651f0b928bccb1ba5162c36b47bb99068beb0a6a
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-09-11 16:48:47 +0500

    ddd

--- madMan/app/DanielGSC/screens/Resturant.tsx
+++ madMan/app/DanielGSC/screens/Resturant.tsx
@@ -1,16 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import {  NativeStackScreenProps } from '@react-navigation/native-stack'
-import { RootParamList } from '../FoodApp'
+import { View, Text } from "react-native";
+import React from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootParamList } from "../FoodApp";
+import ResturanantsCard from "../component/ResturanantsCard";
 
-type Props = NativeStackScreenProps<RootParamList, 'Resturant'>
+type Props = NativeStackScreenProps<RootParamList, "Resturant">;
 
-const Resturant = ({route}:Props) => {
+const Resturant = ({ route, navigation }: Props) => {
   return (
-    <View>
-      <Text>Resturant: {route.params.name}</Text>
+    <View className="p-4">
+      <Text className="text-2xl">{route.params.name}</Text>
+      <Text>Related Resturant</Text>
+      <ResturanantsCard
+        name="Sushi 1"
+        onPress={() => {
+          navigation.push("Resturant", { name: "Sushi 1" });
+        }}
+      />
+      <ResturanantsCard
+        name="Sushi 2"
+        onPress={() => {
+          navigation.push("Resturant", { name: "Sushi 2" });
+        }}
+      />
+      <ResturanantsCard
+        name="Sushi 3"
+        onPress={() => {
+          navigation.push("Resturant", { name: "Sushi 3" });
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Resturant
+export default Resturant;
