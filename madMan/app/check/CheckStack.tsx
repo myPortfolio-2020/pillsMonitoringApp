21393e87a774594dfe43f7c0aec14032fe4739a7
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-09-12 14:09:32 +0500

    done

--- madMan/app/check/CheckStack.tsx
+++ madMan/app/check/CheckStack.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import PgOne from "./PgOne";
 
+const Stack = createNativeStackNavigator();
 const CheckStack = () => {
   return (
-    <View>
-      <Text>CheckStack</Text>
-    </View>
-  )
-}
+    <Stack.Navigator>
+      <Stack.Screen
+        name="PgOne"
+        component={PgOne}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
 
-export default CheckStack
+export default CheckStack;
--- madMan/app/check/PgOne.tsx
+++ madMan/app/check/PgOne.tsx
@@ -1,7 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useGetPatientsQuery } from '../redux/slices/api';
 
 const PgOne = () => {
+
+  const { data } = useGetPatientsQuery();
+  console.log(data)
   return (
     <View>
       <Text>PgOne</Text>
