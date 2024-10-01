ac9bee3470a864644668a376c566ae2908221631
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-10-01 13:48:08 +0500

    ggg

--- PillsTrackFrontEnd/app/navigation/StackNavigation.tsx
+++ PillsTrackFrontEnd/app/navigation/StackNavigation.tsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import Splash from '../screens/Splash';
+
+const Stack = createStackNavigator();
 
 const StackNavigation = () => {
   return (
-    <View>
-      <Text>StackNavigation</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Splash" component={Splash} />
+    </Stack.Navigator>
   )
 }
 
