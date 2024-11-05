01b5ee61ece9ce1230e785a54a0a687ea35026df
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-11-05 11:39:37 +0500

    add Shadcn ui

--- codeGyan/FullStack/elib_dashboard/src/App.tsx
+++ codeGyan/FullStack/elib_dashboard/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <h1>Welcome to my dashboard</h1>
+      <Button variant={"link"}>asf</Button>
     </>
   );
 }
