dd3bc7a3695a58ed361f785c2128f2db1ae9f4cb
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-12-24 11:24:28 +0500

    hj

--- ChaiOurCodeBackEnd/backEnd/src/index.ts
+++ ChaiOurCodeBackEnd/backEnd/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello server!");
+import dotenv from "dotenv";
+import connectDB from "../src/db/index";
+import app from "./app";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log();
+    });
+  })
+  .catch((err) => {
+    console.log("error connection:", err);
+  });
