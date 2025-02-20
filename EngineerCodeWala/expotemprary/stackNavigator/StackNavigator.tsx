b13823f7fa9da1679b7747137c3b0ca6cfaf10ef
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-02-20 17:12:19 +0500

    sd

--- EngineerCodeWala/expotemprary/stackNavigator/StackNavigator.tsx
+++ EngineerCodeWala/expotemprary/stackNavigator/StackNavigator.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import ScreenA from "@/screens/ScreenA";
+import ScreenB from "@/screens/ScreenB";
+
+const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <View>
-      <Text>StackNavigator</Text>
-    </View>
-  )
-}
+    <Stack.Navigator>
+      <Stack.Screen name="ScreenA" component={ScreenA} />
+      <Stack.Screen name="ScreenB" component={ScreenB} />
+    </Stack.Navigator>
+  );
+};
 
-export default StackNavigator
+export default StackNavigator;
