fc4669071226361dec7232d48d0e1f9ca2b03840
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-02-28 19:34:13 +0500

    ddd

--- EngineerCodeWala/expotemprary/bottomNav/bottomScreens/Products.tsx
+++ EngineerCodeWala/expotemprary/bottomNav/bottomScreens/Products.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 const Products = () => {
+  const [bgColor, setBgColor] = useState<string>("red");
   return (
     <View>
-      <Text>Products</Text>
+      <View style={[style.box, { backgroundColor: bgColor }]}>
+        <Text>Products nnn</Text>
+      </View>
+      <TouchableOpacity onPress={() => setBgColor("green")}>
+        <Text>ChangeColor</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
+
+const style = StyleSheet.create({
+  box: {
+    width: 200,
+    height: 200,
+    margin: 12,
+    borderColor: "red",
+  },
+});
