0d7c0c4217222fa40085f933bbfacf787796b3e5
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-03-03 23:49:18 +0500

    ooo

--- EngineerCodeWala/expotemprary/components/UseRefExample.tsx
+++ EngineerCodeWala/expotemprary/components/UseRefExample.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState, useRef } from "react";
 
 const UseRefExample = () => {
+  const [count, setCount] = useState<number>(0);
   return (
     <View>
-      <Text>UseRefExample</Text>
+      <Text>Use of useRef</Text>
+      <View>
+        <Text>1 - prevent unnecesary rerenders</Text>
+        <Text>2 - we can store the previous value of state</Text>
+        <Text>3 - we can access DOM elements</Text>
+      </View>
+      <View>
+        <Text>1 - prevent unnecesary rerenders</Text>
+        <Text>you have add {count} to make it {count +1}</Text>
+        <TouchableOpacity onPress={()=>setCount(count+1)}>
+          <Text>Increase Count</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
