142cf8602c607e83a2d231e2885b008d9f388366
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-04-10 18:47:27 +0500

    ddd

--- SheryiansCoding/backend/index.js
+++ SheryiansCoding/backend/index.js
@@ -1,10 +1,20 @@
 var express = require("express");
+var path = require("path");
 // import express from "express";
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/:harsh", function (req, res) {
-    res.send("Hello World!");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+    res.render("index.ejs");
+});
+app.get("/profile/:username", function (req, res) {
+    res.send("welcome ".concat(req.params.username));
+});
+app.get("/author/:userNm/:age", function (req, res) {
+    //   res.send(req.params);
+    res.send("welcome, ".concat(req.params.userNm, " - I know your age is ").concat(req.params.age));
 });
 app.listen(3000, function () {
     console.log("is it running?");
--- SheryiansCoding/backend/index.ts
+++ SheryiansCoding/backend/index.ts
@@ -1,13 +1,28 @@
 var express = require("express");
+var path = require("path");
 // import express from "express";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/:harsh", (req, res) => {
-  res.send(`Hello World!`);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/profile/:username", (req, res) => {
+  res.send(`welcome ${req.params.username}`);
+});
+
+app.get("/author/:userNm/:age", (req, res) => {
+  //   res.send(req.params);
+  res.send(
+    `welcome, ${req.params.userNm} - I know your age is ${req.params.age}`
+  );
 });
 
 app.listen(3000, () => {
