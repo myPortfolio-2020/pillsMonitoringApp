2d95196e24bac9dbc8dd9c1cd44f17bbb24c1198
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-05-13 18:12:56 +0500

    fas

--- codeGyan/masterAPIs/src/book/bookController.ts
+++ codeGyan/masterAPIs/src/book/bookController.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
-export const createBook = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {};
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  console.log("files", req.files);
+  res.json({});
+};
+
+export { createBook };
--- codeGyan/masterAPIs/src/book/bookRouter.ts
+++ codeGyan/masterAPIs/src/book/bookRouter.ts
@@ -7,18 +7,22 @@ const bookRouter = express.Router();
 
 //routes
 // api/books
-// remeber his diagram, multer is a middlewate that can be define as in between the path and controller 
+// remeber his diagram, multer is a middlewate that can be define as in between the path and controller
 // bookRouter.post("/", ()=>{} ,createBook);
 // and we are using multer
 
-const upload  = multer({
-    dest: path.resolve(__dirname,'../../public/data/uploads'),
-    limits: {fileSize: 3e7} //30mb
-})
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 }, //30mb
+});
 
-bookRouter.post("/", upload.fields([
-    {name: "coverImage" , maxCount:1}, //coverImage is given in our model
-    {name: "file", maxCount:1} // file is given in our model
-]),createBook);
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 }, //coverImage is given in our model
+    { name: "file", maxCount: 1 }, // file is given in our model
+  ]),
+  createBook
+);
 
 export default bookRouter;
