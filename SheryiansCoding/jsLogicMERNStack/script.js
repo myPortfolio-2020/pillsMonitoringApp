da4341a03e45bf2f4bd755b00b6c161e930209a6
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-05-14 18:11:04 +0500

    sdf

---
+++ SheryiansCoding/jsLogicMERNStack/script.js
@@ -0,0 +1,16 @@
+// https://www.youtube.com/watch?v=2vVq1xXIL58&t=3s
+
+const names = [
+  "John",
+  "Emma",
+  "Michael",
+  "Sarah",
+  "David",
+  "Lisa",
+  "James",
+  "Emily",
+  "Daniel",
+  "Sophia",
+];
+
+names.forEach
