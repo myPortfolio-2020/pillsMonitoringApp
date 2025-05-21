9a6caaecf48d93bd45a47ba55665ce6345468ab6
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-05-21 17:16:34 +0500

    eid

--- codeGyan/masterAPIs/src/book/bookController.ts
+++ codeGyan/masterAPIs/src/book/bookController.ts
@@ -1,7 +1,26 @@
-import {  Request, Response } from "express";
+import path from "node:path";
+import { Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
 
 const createBook = async (req: Request, res: Response) => {
   console.log("files", req.files);
+
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+
+  const uploadResult = await cloudinary.uploader.upload(filePath, {
+    filename_override: fileName,
+    folder: "book-covers",
+    format: coverImageMimeType,
+  });
+
+  console.log("uploadResult:", uploadResult);
   res.json({});
 };
 
