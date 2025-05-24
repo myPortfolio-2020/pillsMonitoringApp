08b9e6fbdd15e1a4408ebe41548440a98a4efb74
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-05-24 10:23:42 +0500

    ASD

---
+++ ChaiOurCodeBackEnd/backEnd/js.ts
@@ -0,0 +1,37 @@
+// // let x = false
+// // let xWhat = Number(x)
+
+// // console.log(x, typeof(x))
+// // console.log(xWhat, typeof (xWhat), xWhat * xWhat)
+
+// let x = 0;
+// let xWhat = Boolean(x);
+
+// // console.log(x, typeof x);
+// // console.log(xWhat, typeof xWhat, xWhat * xWhat);
+
+// // let gameCounter = 100;
+// // gameCounter++;
+// // gameCounter++;
+// // console.log(gameCounter);
+
+// let a = 4
+// let b = 4
+
+// console.log('a =  b' ,a === b)
+
+// let x1 = Symbol(5)
+// let y1 = Symbol(5);
+
+// console.log('symbol', x1 === y1)
+
+
+let x = 7
+let b = x
+
+console.log(x, b)
+
+let c = [2, 3, 4]
+let d = c
+
+console.log(c)
