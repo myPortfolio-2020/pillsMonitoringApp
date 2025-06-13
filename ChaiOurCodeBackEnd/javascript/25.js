13b151265be75a074c833f496fd368cbe54eff84
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-13 17:20:03 +0500

    sdsd

---
+++ ChaiOurCodeBackEnd/javascript/25.js
@@ -0,0 +1,5 @@
+// ------------------Execution Context -------------
+// 1- Global Execution Context  (this - browser - windows)
+// 2- Function Execution Context
+// 3- Eval 
+
---
+++ ChaiOurCodeBackEnd/javascript/react.js
@@ -0,0 +1,52 @@
+// 1. What is React?
+// answer: A. A JavaScript framework for building APIs
+
+
+// 2. What is the correct way to create a component in React?
+// A
+import react from 'react'
+
+const MyComponent = ()=>{
+    return <>
+        <div>MyComponent</div>
+    </>
+}
+
+export default MyComponent
+
+
+// 3. What is JSX? (Choose one)
+// A. JavaScript XML — allows writing HTML in JavaScript
+
+// 4. Which hook is used to manage state in a functional component?
+// B. useState()
+
+// 5. True or False: In React, every component must return a single root element.
+// true
+
+
+// 6. What will this component render?
+// Hello, Jane!
+
+// 7. Fill in the blank:
+// To handle side effects like API calls in React, you use the _useEffect()_______ hook.
+
+// 8. Which of the following is NOT a valid lifecycle method in class components?
+// B. componentWillUnmount()
+
+// 9. Write a simple functional component named Welcome that renders: "Welcome to React!"
+
+import react from 'react'
+
+const Welcome = ()=>{
+    return <>
+        <div>Welcome to React!</div>
+    </>
+}
+
+export default Welcome
+
+// 10. What does the following code do?
+// const [count, setCount] = useState(0);
+// set the value of count as 0
+
