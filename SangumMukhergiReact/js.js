10451666bb7b8801967077a6f3e2c2657e78333a
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-18 18:24:37 +0500

    done

--- SangumMukhergiReact/js.js
+++
@@ -1,9 +0,0 @@
-const restFn01 = () => {
-  return "NO PARAMETER";
-};
-console.log(restFn01(2, 4, 4, 5)); // NO PARAMETER
-
-const restFn02 = (...arg) => {
-  return arg;
-};
-console.log(restFn02(2, 4, 4, 5)); // [ 2, 4, 4, 5 ]
--- SangumMukhergiReact/spreadAndRest.js
+++ SangumMukhergiReact/spreadAndRest.js
@@ -2,7 +2,7 @@ let x = [1, 2, 3, 4, 5];
 let y = [6, 7, 8, 9, 10];
 
 x.push(y); // 6 element
-let ifPush = x.push(y); 
+let ifPush = x.push(y);
 console.log(ifPush); // 7 elements
 
 console.log("x: ", x); // x:  [ 1, 2, 3, 4, 5, [ 6, 7, 8, 9, 10 ], [ 6, 7, 8, 9, 10 ] ]
@@ -17,3 +17,18 @@ console.log("x2:", x2); // x2: [ 1, 2, 3, 4, 5 ]
 console.log("y2:", y2); // y2: [ 6, 7, 8, 9, 10 ]
 
 /* REST OPERATORS */
+const restFn01 = () => {
+  return "NO PARAMETER";
+};
+console.log(restFn01(2, 4, 4, 5)); // NO PARAMETER
+
+const restFn02 = (...arg) => {
+  return arg;
+};
+console.log(restFn02(2, 4, 4, 5)); // [ 2, 4, 4, 5 ]
+
+let restFn4 = (a, b, ...c) => {
+  // console.log(a,b,c)
+  return [a, b, c];
+};
+console.log(restFn4(1, 2, 45, 56, 87, 45, 11)); // [ 1, 2, [ 45, 56, 87, 45, 11 ] ]
