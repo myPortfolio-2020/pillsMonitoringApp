a742332991e03260630a8040722922031934428f
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-18 17:29:18 +0500

    sss

--- SangumMukhergiReact/js.js
+++ SangumMukhergiReact/js.js
@@ -1,9 +1,2 @@
-let x = [1, 2, 3, 4, 5];
-let y = [6, 7, 8, 9, 10];
-
-x.push(y); 
-let ifPush = x.push(y); 
-console.log(ifPush) // 7
-
-console.log("x: ", x); // x:  [ 1, 2, 3, 4, 5, [ 6, 7, 8, 9, 10 ], [ 6, 7, 8, 9, 10 ] ]
-console.log("y: ", y); // y:  [ 6, 7, 8, 9, 10 ]
+let x3 = [1, 2, 3, 4, 5];
+let y3 = [6, 7, 8, 9, 10];
--- SangumMukhergiReact/spreadAndRest.js
+++ SangumMukhergiReact/spreadAndRest.js
@@ -1,9 +1,17 @@
 let x = [1, 2, 3, 4, 5];
 let y = [6, 7, 8, 9, 10];
 
-x.push(y);
-let ifPush = x.push(y);
-console.log(ifPush); // 7
+x.push(y); // 6 element
+let ifPush = x.push(y); 
+console.log(ifPush); // 7 elements
 
 console.log("x: ", x); // x:  [ 1, 2, 3, 4, 5, [ 6, 7, 8, 9, 10 ], [ 6, 7, 8, 9, 10 ] ]
 console.log("y: ", y); // y:  [ 6, 7, 8, 9, 10 ]
+
+// rest and spread
+let x2 = [1, 2, 3, 4, 5];
+let y2 = [6, 7, 8, 9, 10];
+
+console.log([...x2, ...y2]); // [  1, 2, 3, 4,  5,  6, 7, 8, 9, 10]
+console.log("x2:", x2); // x2: [ 1, 2, 3, 4, 5 ]
+console.log("y2:", y2); // y2: [ 6, 7, 8, 9, 10 ]
