a043da32ed03f932e055a6a9e8b15a7b41879359
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-19 15:50:01 +0500

    jj

--- React19StepByStep/react19/src/App.jsx
+++ React19StepByStep/react19/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Condition from "./conditionEx1/Condition";
 import Example1 from "./HooksByWDM/useLayoutEffect/Example1";
 function App() {
   return (
     <>
       <div>Hi, world!!!</div>
-
+      <Condition />
     </>
   );
 }
---
+++ React19StepByStep/react19/src/conditionEx1/Condition.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const building = [
+  {
+    bed: 1,
+    door: 1,
+    cat: "Studio",
+  },
+  {
+    bed: 2,
+    door: 2,
+    cat: "small house",
+  },
+  {
+    bed: 3,
+    door: 3,
+    cat: "Dream house",
+  },
+];
+
+function Condition() {
+  const [condition, setCondition] = useState(building);
+
+  const beds = condition.map((item) => item.bed);
+console.log(beds)
+  const checkHouses = () => {
+    console.log("house selection");
+  };
+  console.log(condition);
+  return (
+    <>
+      <div>
+        <h2>Check Conditions</h2>
+        <div>which size you want to have?</div>
+        <div>
+          <button onClick={() => checkHouses()}>One</button>
+          <button onClick={() => checkHouses()}>Two</button>
+          <button onClick={() => checkHouses()}>Three</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Condition;
