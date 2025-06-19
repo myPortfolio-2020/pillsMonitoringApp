889428c707f22e12f42774dc6f9b2c61084f9692
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-19 09:55:17 +0500

    lll

--- mysterymessage/src/schemas/MessageSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const MessageSchema = z.object({
-  content: z
-    .string()
-    .min(10, { message: "must be 10 char" })
-    .max(20, { message: "must be no longer than 300" }),
-});
--- mysterymessage/src/schemas/SignInSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-// identifier = email/username
-export const SignInSchema = z.object({
-  identifier: z.string(),
-  password: z.string(),
-});
