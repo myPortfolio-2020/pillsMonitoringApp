ab9e315a4589dfcfcf1c9a8ec332e7c5e8b51a5c
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-23 17:33:01 +0500

    yyyy

--- React19StepByStep/react19/src/App.jsx
+++ React19StepByStep/react19/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Parent from "./propsExamples/Parent";
+import CondiPreferences from "./whichConditions/CondiPreferences";
 
 function App() {
   return (
     <>
       <div>Hi, world!!!</div>
-      <Parent />
+      <CondiPreferences />
     </>
   );
 }
---
+++ React19StepByStep/react19/src/whichConditions/CondiPreferences.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CondiPreferences() {
+  return (
+    <>
+      <div>CondiPreferences</div>
+      <div>either, && || , if() , itinary operators</div>
+    </>
+  );
+}
+
+export default CondiPreferences;
