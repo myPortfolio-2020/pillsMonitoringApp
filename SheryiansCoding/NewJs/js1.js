7a63243cad484aace7941f80e71420476db931df
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-30 12:47:30 +0500

    kk

---
+++ SheryiansCoding/NewJs/js1.js
@@ -0,0 +1,22 @@
+// !!0
+// falsy value
+// truthy value
+
+// falsy value
+// !!0
+// false
+// !!undefined
+// false
+// !!null
+// false
+// !!false
+// false
+// !!""
+// !!document.all
+// false
+// NaN
+// false
+
+// if (NaN) {
+//   console.log("this is considered as falsy");
+// }
