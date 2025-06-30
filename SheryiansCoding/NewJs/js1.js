c19a094597d578d7b7a703f868c102915956cece
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-06-30 15:05:44 +0500

    wdddd

--- SheryiansCoding/NewJs/js1.js
+++ SheryiansCoding/NewJs/js1.js
@@ -17,6 +17,22 @@
 // NaN
 // false
 
-// if (NaN) {
-//   console.log("this is considered as falsy");
-// }
+if (NaN) {
+  console.log("this is considered as falsy");
+}
+
+// rest of all will convert into true
+if (-1) {
+  console.log("it will convert into true (-1 === true)");
+}
+// predict
+// 
+
+let x = 100
+let y = 200
+
+let z = x < 20 || y > 100
+
+// false true
+
+console.log(z)
