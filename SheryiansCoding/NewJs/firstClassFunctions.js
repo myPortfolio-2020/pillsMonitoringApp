d78c456d6d09ad6b55225a226a8985461b8ffa54
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-07-01 12:40:46 +0500

    ddd

--- SheryiansCoding/NewJs/firstClassFunctions.js
+++ SheryiansCoding/NewJs/firstClassFunctions.js
@@ -1,9 +1,27 @@
 // First Class Functions
+// Functions can be assigned to variables.
+// Functions can be passed as arguments.
+// Functions can be returned from other functions.
+
+// --------------------------------------------------------------
 
 const firstClassFn = (val) => {
-   return val();
+  return val();
 };
 
 firstClassFn(function () {
   console.log("hi, I am first class function");
 });
+
+// --------------------------------------------------------------
+
+const firstClassFn2 = (fcfn2) => {
+  return fcfn2();
+};
+
+firstClassFn2(() => {
+  console.log("I am another example of fcf");
+});
+
+// --------------------------------------------------------------
+
