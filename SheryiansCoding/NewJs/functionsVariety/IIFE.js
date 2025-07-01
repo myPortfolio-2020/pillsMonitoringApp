6d40110a352a87920bd5d2c3baa99b91a5261946
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-07-01 16:13:44 +0500

    iife

--- SheryiansCoding/NewJs/functionsVariety/IIFE.js
+++ SheryiansCoding/NewJs/functionsVariety/IIFE.js
@@ -1,2 +1,17 @@
 // IIFE
 // Immediate Invoked Function execution
+
+
+(function(){
+    console.log('IIFE')
+})()
+
+(function() {
+    var name = 'ChatGPT';
+    console.log('Hello, ' + name);
+})();
+
+// Arrow Function
+(() => {
+    console.log('Arrow IIFE');
+})();
