3e297caec3902c9c3af69d109852f1e1fdee1763
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-07-02 14:37:54 +0500

    ddd

---
+++ SheryiansCoding/NewJs/array/array.js
@@ -0,0 +1,41 @@
+// array
+// push gives length
+// pop return the poped item
+
+// push
+//No, the .push() method does not return a new array in JavaScript. It modifies the original array in place and returns the new length of the array.
+let walnut = [1, 2, 3, 4, 5];
+let walnutPush = walnut.push(6);
+
+console.log(walnut); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(walnutPush); // 6 (total 6 elements)
+
+//Pop
+let almond = [11, 12, 13, 14, 15];
+let almondPop = almond.pop();
+
+console.log(almond); // [11,12,13,14]
+console.log(almondPop); // 15 (return last item,  15 was poped )
+
+// shift - works like pop but start from 0 index
+let ele = [100, 200, 300, 400, 500];
+const shiftEle = ele.shift();
+
+console.log(ele); // [ 200, 300, 400, 500 ]
+console.log(shiftEle); // 100
+
+// unShift - works like push but start from 0 index -
+// it is called unshift because it place the element in the begining of an array and shift the position of the other elements
+let unShiftEle = ["a", "b", "c", "d", "e"];
+let eleUnshift = unShiftEle.unshift("a+", "b+");
+
+console.log(unShiftEle); // ["a+",'b+',"a","b","c","d","e"]
+console.log(eleUnshift); // 6
+
+
+// Splice
+let spliceMe = [1,2,3,4,5,6,7,8,9]
+let spliced = spliceMe.splice(2,4)
+
+console.log(spliceMe) //
+console.log(spliced) //
--- SheryiansCoding/NewJs/functionsVariety/Closures.js
+++ SheryiansCoding/NewJs/functionsVariety/Closures.js
@@ -1,14 +1,26 @@
 // Closures
 // Function which returns another function and that return function will always use any variable from its parent function
 
-
-const closureFn = ()=>{
-    let a = 12;
-    return ()=>{
-        console.log(a)
-    }
-}
+const closureFn = () => {
+  let a = 12;
+  return () => {
+    console.log(a);
+  };
+};
 
 // closureFn() - NO - Why -you need to call the inner function
-const innerFn = closureFn()
-innerFn()
+const innerFn = closureFn();
+innerFn();
+
+// chatGpt
+// ----------------------------------------------------------------
+// Write a function createCounter that returns another function. Each time the inner function is called, it should increment a counter and return the new value.
+//-----------------------------------------------------------------
+//Write a function multiplyBy that takes a number as an argument and returns a function. That returned function takes another number and multiplies it by the first.
+//-----------------------------------------------------------------
+//Create a function secretHolder(secret) that returns an object with two methods: getSecret and setSecret. These methods should allow access to a private variable.
+//-----------------------------------------------------------------
+//Write a function delayedLogger that takes an array of strings and logs each string after a delay equal to its index * 1000 milliseconds. Use a closure to capture the correct value.
+//-----------------------------------------------------------------
+//Create a function once that accepts another function as input and returns a new function that can only be called once. After the first call, it should return the same result without calling the original function again.
+//-----------------------------------------------------------------
