d70a4ab414b0872cc41ff048968eb291ef5e3f39
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-07-02 15:32:34 +0500

    ddd

--- SheryiansCoding/NewJs/array/splice.js
+++ SheryiansCoding/NewJs/array/splice.js
@@ -7,9 +7,20 @@ let spliced = spliceMe.splice(2, 4);
 console.log(spliceMe); // [ 1, 2, 7, 8, 9 ]
 console.log(spliced); // [ 3, 4, 5, 6 ]
 
-
 //Exercise 1: Remove an Element
 //Remove 'blue' from the array using splice().
 
-let colors = ['red', 'green', 'blue', 'yellow'];
+let colors = ["red", "green", "blue", "yellow"];
+let spliceColor = colors.splice(2, 1);
 
+console.log(colors); // ['red', 'green', 'yellow']
+console.log(spliceColor); // ['blue']
+
+// Exercise 2: Insert Elements
+// Insert 3 and 4 between 2 and 5 using splice().
+
+let numbers = [1, 2, 5, 6];
+let spliceNumbers = numbers.splice()
+
+console.log();
+console.log();
