69b5d9776434bcb4f30d9b2e21ca9a9bd5a90697
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-07-03 17:51:52 +0500

    ddd

--- ReactAnimation/gsapTut/svg/svg.js
+++ ReactAnimation/gsapTut/svg/svg.js
@@ -4,14 +4,21 @@ var finalPath = `M 10 100 Q 500 100 996 100`;
 
 var string = document.querySelector("#string");
 
+//https://gsap.com/docs/v3/Eases/
+
 string.addEventListener("mousemove", function (dets) {
-  path = `M 10 100 Q 500 ${dets.y} 996 100`;
+  path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`;
   gsap.to("svg path", {
     attr: { d: path },
     duration: 0.36,
+    ease: "power3.out",
   });
 });
 
 string.addEventListener("mouseleave", function () {
-  console.log("leave");
+  gsap.to("svg path", {
+    attr: { d: finalPath },
+    duration: 1.5,
+    ease: "elastic.out(1,0.3)",
+  });
 });
